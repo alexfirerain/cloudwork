0dd3c88efe0f38bfde7f677b37782ff7caec88da
Alexander Lukyanov <lux-nova@mail.ru>
2023-03-11 18:02:09 +0300

    Init

---
+++ src/main/java/ru/netology/cloudwork/CloudworkApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.cloudwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/netology/cloudwork/CloudworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.cloudwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
