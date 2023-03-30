a128ea39b39107c4c74900d5090ed3d5da46037d
alexfirerain <alexfirerain@ya.ru>
2023-03-30 18:20:40 +0300

    in details

--- src/main/java/ru/netology/cloudwork/config/SecurityConfig.java
+++ src/main/java/ru/netology/cloudwork/config/SecurityConfig.java
@@ -1,16 +1,24 @@
 package ru.netology.cloudwork.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
 //                .httpBasic().disable()
                 .cors(httpSecurityCorsConfigurer -> {
@@ -33,6 +41,26 @@ public class SecurityConfig {
                 .and()
                 .build();
 
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails user = User.withUsername("storageuser")
+                .password(encoder.encode("0000"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(encoder.encode("9999"))
+                .roles("SUPERUSER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
