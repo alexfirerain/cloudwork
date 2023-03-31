16b6e3febbec504b3aa1bb753ecfd21000cedc8e
alexfirerain <alexfirerain@ya.ru>
2023-03-31 15:03:48 +0300

    in colors

--- src/main/java/ru/netology/cloudwork/entity/UserEntity.java
+++ src/main/java/ru/netology/cloudwork/entity/UserEntity.java
@@ -2,13 +2,16 @@ package ru.netology.cloudwork.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Data
 @Entity
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +26,28 @@ public class UserEntity {
     @OneToMany(mappedBy ="owner", fetch = FetchType.EAGER)
     private List<FileEntity> files;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
