fb10092ebcd99975dbb235ed535297da5a33c0d8
alexfirerain <alexfirerain@ya.ru>
2023-04-11 14:17:16 +0300

    v 0.1.7b

--- src/main/java/ru/netology/cloudwork/repository/FileRepository.java
+++ src/main/java/ru/netology/cloudwork/repository/FileRepository.java
@@ -3,6 +3,7 @@ package ru.netology.cloudwork.repository;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.netology.cloudwork.entity.FileEntity;
 import ru.netology.cloudwork.entity.UserEntity;
 
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface FileRepository extends JpaRepository<FileEntity, Long> {
 
     List<FileEntity> findByOwner(@NotNull UserEntity owner);
--- src/main/java/ru/netology/cloudwork/repository/UserRepository.java
+++ src/main/java/ru/netology/cloudwork/repository/UserRepository.java
@@ -2,11 +2,13 @@ package ru.netology.cloudwork.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.netology.cloudwork.entity.UserEntity;
 
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
 }
